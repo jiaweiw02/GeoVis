1b75ece1b0ef4f8008fb390040e74cf7da86e794
Jiawei Wu <wuj22@rpi.edu>
2024-04-03 03:13:59 -0400

    plotted points and tooltip appearing right next to i!!!!

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const width = 975;
 const height = 615;
+const circleSize = 5;
+const hoveringCircleSize = 10;
+const pointColor = "blue";
 
 d3.json("./states-albers-10m.json", function(error, us) {
 
@@ -26,6 +29,18 @@ d3.json("./states-albers-10m.json", function(error, us) {
         .attr('stroke-linecap', 'round')
         .attr('d', path(topojson.mesh(us, us.objects.states, (a,b) => a !== b)));
     
+    // edit tooltip style here
+    var tooltip = d3.select('body').append('div')
+        .style('display', 'inline')
+        .style('position', 'fixed')
+        .style('visibility', 'hidden')
+        .style('opacity', 0)
+        .style('background-color', 'white')
+        .style('border', 'solid')
+        .style('border-width', '1px')
+        .style('border-radius', '5px')
+        .style('padding', '10px');
+    
     // plot the points
     d3.json("./data.json", function(error, data) {
         if (error) throw error;
@@ -35,12 +50,37 @@ d3.json("./states-albers-10m.json", function(error, us) {
             .enter().append("circle")
             .attr("cx", function(d) { return projection([d.Longitude, d.Latitude])[0]; })
             .attr("cy", function(d) { return projection([d.Longitude, d.Latitude])[1]; })
-            .attr("r", 5)
-            .attr("fill", "red")
-            .append("title")
-            .text(function(d) { return d.Information; });
+            .attr("r", circleSize)
+            .attr("fill", pointColor) // change the point color
+            .on("mouseover", function() {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr("r", hoveringCircleSize);
+            })
+            .on("mousemove", function() {
+                tooltip.style('top', (d3.mouse(this)[1] - 10) + 'px')
+                    .style('left', (d3.mouse(this)[0] + 40) + 'px');
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .attr("r", circleSize);
+                tooltip.transition()
+                    .duration(300)
+                    .style('opacity', 0)
+                    .style('visibility', 'hidden');
+            })
+            .on("click", function(d) {
+                tooltip.html(d.Information)
+                    .style('visibility', 'visible')
+                    
+                    .transition()
+                    .duration(300)
+                    .style('opacity', 1);
+            })
     });
-
 
     document.body.appendChild(svg.node());
 })
