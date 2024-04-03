4c122ff03590b490a9ad2527b362e26a638665a4
jiaweiw02 <wuj22@rpi.edu>
2024-04-03 02:22:32 -0400

    points plotted

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-// import us from './states-albers-10m.json';
-
 const width = 975;
 const height = 615;
 
 d3.json("./states-albers-10m.json", function(error, us) {
+
+    if (error) throw error;
+
     const svg = d3.select('body').append('svg')
-    .attr('viewBox', `0 0 ${width} ${height}`)
-    .attr('height', height)
-    .attr('width', width);
+        .attr('height', height)
+        .attr('width', width);
+
+    const projection = d3.geoAlbersUsa()
+        .translate([width / 2, height / 2])
+        .scale(1300);
 
     const path = d3.geoPath();
 
-    svg.append('path')
+    const USbackground = svg.append('path')
         .attr('fill', '#ddd')
         .attr('d', path(topojson.feature(us, us.objects.nation)));
 
-    svg.append('path')
+    const borders = svg.append('path')
         .attr('fill', 'none')
         .attr('stroke', '#fff')
         .attr('stroke-linejoin', 'round')
-        .attr('stroke-linecap', 'round');
+        .attr('stroke-linecap', 'round')
+        .attr('d', path(topojson.mesh(us, us.objects.states, (a,b) => a !== b)));
+    
+    // plot the points
+    d3.json("./data.json", function(error, data) {
+        if (error) throw error;
+
+        svg.selectAll("circle")
+            .data(data)
+            .enter().append("circle")
+            .attr("cx", function(d) { return projection([d.Longitude, d.Latitude])[0]; })
+            .attr("cy", function(d) { return projection([d.Longitude, d.Latitude])[1]; })
+            .attr("r", 5)
+            .attr("fill", "red")
+            .append("title")
+            .text(function(d) { return d.Information; });
+    });
 
 
     document.body.appendChild(svg.node());
